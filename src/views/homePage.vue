54dfc88f6f7b75cc304d762b1dc8bc6d7a1bd64a
satinder <satindersingh472@gmail.com>
2022-09-22 15:27:37 -0600

    started

---
+++ src/views/homePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>This is the news page
+        </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
